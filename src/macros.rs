755a58e1fd341b7ed6363977b83859c5644dd25e
vyfor <vyfor@proton.me>
2026-04-14 15:01:19 +0500

    feat(macros): generate docs for presets

--- src/macros.rs
+++ src/macros.rs
@@ -17,14 +17,14 @@ macro_rules! rattle {
     };
 
     (@impl $ty:ident, $fn_name:ident, $width:literal, $height:literal, $interval_ms:literal, @inline [$($frame:literal),+]) => {
-        $crate::rattle!(@emit $ty, $fn_name, $width, $height, $interval_ms, &[$(&[$frame]),+]);
+        $crate::rattle!(@emit $ty, $fn_name, $width, $height, $interval_ms, &[$(&[$frame]),+], [$($frame),+]);
     };
 
     (@impl $ty:ident, $fn_name:ident, $width:literal, $height:literal, $interval_ms:literal, @path $frames:path) => {
-        $crate::rattle!(@emit $ty, $fn_name, $width, $height, $interval_ms, $frames);
+        $crate::rattle!(@emit $ty, $fn_name, $width, $height, $interval_ms, $frames, $frames);
     };
 
-    (@emit $ty:ident, $fn_name:ident, $width:literal, $height:literal, $interval_ms:literal, $frames:expr) => {
+    (@emit $ty:ident, $fn_name:ident, $width:literal, $height:literal, $interval_ms:literal, $frames:expr, $docs:tt) => {
         #[derive(Debug, Clone, Copy, Default)]
         pub struct $ty;
 
@@ -35,6 +35,7 @@ macro_rules! rattle {
             const FRAMES: &'static [&'static [&'static str]] = $frames;
         }
 
+        #[doc = concat!("```rust\n", stringify!($docs), "\n```")]
         pub fn $fn_name() -> $crate::Rattler<$ty> {
             $crate::Rattler::new()
         }
